ac2bb99a9688302cb7206977918dbca091d7e1eb
francis.phillip@student.greenriver.edu <155685161+PhillipFrancis@users.noreply.github.com>
2024-01-19 11:20:41 -0800

    Update

--- start/Hello.py
+++ start/Hello.py
@@ -1,14 +1,18 @@
 # A sinple "hello word" script in python.
 # Created by Phillip Francis
 
-print('hello world')
-message = "hello there"
-n = 17
-pi = 3.1415926535
-print(message)
-print(n + pi)
-print(pi * n)
+#print('hello world')
+#message = "hello there"
+#n = 17
+#pi = 3.1415926535
+#print(message)
+#print(n + pi)
+#print(pi * n)
 
-your_name = input('what is your name')
+your_name = input('what is your name: ')
+num = int(input('How old are you: '))
+
 print('Hello ' + your_name)
 print('today is going to be a great day ... FOR A WHITE WEDDING!')
+print('in two years you will be:')
+print(num + 2)
